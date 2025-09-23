44eb9b9c05dab9fd199b918b4c2b9689ecc8d45f
enesvurgun <143536187+enesvurgun@users.noreply.github.com>
2025-09-23 13:52:09 +0300

    Add files via upload

---
+++ sunucu-licence/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+const validKeys = (process.env.LICENSE_KEYS || "").split(",");
+
+// Ana doğrulama adresi
+app.get("/validate", (req, res) => {
+  const providedKey = req.query.license;
+
+  if (!providedKey) {
+    return res.status(400).json({ status: "error", message: "No license key provided." });
+  }
+
+  if (validKeys.includes(providedKey)) {
+    return res.status(200).json({ status: "success", message: "License is valid." });
+  } else {
+    return res.status(403).json({ status: "error", message: "Invalid license key." });
+  }
+});
+
+// Render'ın bize vereceği portu dinle
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
