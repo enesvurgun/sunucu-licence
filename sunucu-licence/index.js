cd0f571bafec3caee93a538554b2dbf08fbe9f0a
enesvurgun <143536187+enesvurgun@users.noreply.github.com>
2025-09-24 13:49:16 +0300

    Add files via upload

--- sunucu-licence/index.js
+++ sunucu-licence/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const crypto = require('crypto'); 
+const crypto = require('crypto');
 
 app.use(cors());
 app.use(express.json());
 
 const validKeys = (process.env.LICENSE_KEYS || "").split(",");
-const sessionKeys = new Map(); 
+const sessionKeys = new Map();
 const ALGORITHM = 'aes-256-ctr';
 const IV_LENGTH = 16;
 
@@ -36,31 +36,9 @@ const functionLibrary = {
                     body: params.toString()
                 });
                 if (!response.ok) throw new Error("Imperia sunucu hatası: " + response.status);
-                console.log("Ordu taşıma isteği başarıyla gönderildi.");
+                console.log("✅ Ordu taşıma isteği başarıyla gönderildi.");
             } catch (error) {
-                console.error("İstek sırasında bir hata oluştu:", error);
-            }
-        }
-    `,
-    addGovernorXp: `
-        async function(personId, xpAmount) {
-            const params = new URLSearchParams();
-            params.append("xjxfun", "useItem");
-            params.append("xjxr", Date.now().toString());
-            const governorPayload = \`<xjxobj><e><k>governorXp</k><v>S\${xpAmount}</v></e><e><k>personId</k><v>S\${personId}</v></e><e><k>type</k><v>S4</v></e><e><k>tab</k><v>S1</v></e><e><k>realContainerId</k><v>SPalacePersonProfile\${personId}</v></e></xjxobj>\`;
-            params.append("xjxargs[]", governorPayload);
-            params.append("xjxargs[]", \`<xjxobj><e><k>vexok</k><v>Btrue</v></e></xjxobj>\`);
-            params.append("xjxargs[]", \`<xjxobj><e><k>isMobileView</k><v>Bfalse</v></e></xjxobj>\`);
-            try {
-                const response = await fetch("https://www.imperiaonline.org/imperia/game_v6/game/xajax_loader.php", {
-                    method: "POST",
-                    headers: { "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8" },
-                    body: params.toString()
-                });
-                if (!response.ok) throw new Error("Imperia sunucu hatası: " + response.status);
-                console.log("Vali XP isteği başarıyla gönderildi.");
-            } catch (error) {
-                console.error("İstek sırasında bir hata oluştu:", error);
+                console.error("❌ İstek sırasında bir hata oluştu:", error);
             }
         }
     `
